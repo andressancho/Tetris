56f59221938e477e821ff803973be7f899c3dbdb
Andres Sancho <andrem.san12@gmail.com>
2018-03-04 18:53:50 -0600

    Tercer commit

--- app/src/main/java/andressancho/com/tetris/MainActivity.java
+++ app/src/main/java/andressancho/com/tetris/MainActivity.java
@@ -1,12 +1,12 @@
 package andressancho.com.tetris;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayout;
+import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -17,23 +17,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridLayout gl = findViewById(R.id.gl);
-        ArrayList<View> views= new ArrayList<View>();
 
-        for(int x=0;x<13;x++){
-            for(int y=0;y<18;y++){
-                ImageView iv= new ImageView(this);
-                iv.setTag(Integer.toString(x)+Integer.toString(y));
-                iv.setImageResource(R.drawable.pieza);
-                ViewGroup.LayoutParams params = iv.getLayoutParams();
-                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-                params.height = ViewGroup.LayoutParams.MATCH_PARENT;
-                iv.setLayoutParams(params);
-                views.add(iv);
-            }
-
-        }
-        gl.addChildrenForAccessibility(views);
+        initTablero();
 
     }
+    public void initTablero(){
+        GridLayout gl = findViewById(R.id.gl);
+
+        for(int x=0;x<20;x++){
+            for(int y=0;y<13;y++){
+                ImageView iv= new ImageView(this);
+                iv.setTag(Integer.toString(x)+Integer.toString(y));
+                //iv.setImageResource(R.drawable.pieza);
+                gl.addView(iv);
+            }
+        }
+    }
 }
+
