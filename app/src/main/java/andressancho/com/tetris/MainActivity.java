54273b786223d3951b46b7cc579df7d7c6fbdeb3
Andres Sancho <andrem.san12@gmail.com>
2018-03-05 23:08:36 -0600

    Avance

--- app/src/main/java/andressancho/com/tetris/MainActivity.java
+++ app/src/main/java/andressancho/com/tetris/MainActivity.java
@@ -3,14 +3,20 @@ package andressancho.com.tetris;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<ArrayList<Integer>> tablero= new ArrayList<ArrayList<Integer>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
         initTablero();
 
+
     }
     public void initTablero(){
         GridLayout gl = findViewById(R.id.gl);
 
         for(int x=0;x<20;x++){
+            tablero.add(new ArrayList<Integer>());
             for(int y=0;y<13;y++){
+                tablero.get(x).add(0);
                 ImageView iv= new ImageView(this);
                 iv.setTag(Integer.toString(x)+Integer.toString(y));
-                //iv.setImageResource(R.drawable.pieza);
+
+                iv.setImageResource(R.drawable.pieza_verde);
                 gl.addView(iv);
+
             }
         }
+        Log.d("d",tablero.toString());
+
+        T
     }
 }
 
